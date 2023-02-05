a5e91dc038d2c1aed545cef205ebc93706cb91ce
Your Name <you@example.com>
2023-02-05 17:21:30 -0600

    add ablility to substitute an item from meal plan

--- mealPlan.py
+++ mealPlan.py
@@ -19,31 +19,37 @@ def main():
    mealListVeg = []
    mealListMeat = []
    mealPlan = []
+   mealPicks = []
    for meals in mealsListDict:
      if meals["type"] == "dinner":
          if meals["vegetarian"] == "yes":
            mealListVeg.append(meals["name"])
          else:
            mealListMeat.append(meals["name"])
-   mealPicksVeg = mpf.genmealplan(mealListVeg, 4)
+   mealPicksVeg = mpf.genmealplan(mealListVeg, (4+1))
    tuesdaysPick = mealPicksVeg[0]
-   mealPicksMeat = mpf.genmealplan(mealListMeat, 3)
-   mealPicks = mealPicksVeg[1:] + mealPicksMeat
+   mealPicksMeat = mpf.genmealplan(mealListMeat, (3+1))
+   mealPicks = mealPicksVeg[1:4] + mealPicksMeat[0:3]
+   #mealPicks = mealPicksVeg[1:3]
+   #[mealPicks.append(item) for item in mealPicksMeat[0:2]]
    random.shuffle(mealPicks)
    mealPlan.append(mealPicks[0])
    mealPlan.append(tuesdaysPick)
-   mealPlan = mealPlan + mealPicks[1:]
-   print(mealPlan)
-
+   mealPlan = mealPlan + mealPicks[1:6]
+   #[mealPlan.append(item) for item in mealPicks[1:5]]
+   print(mealPlan) #for test
+   
+   #request substitute meals 
+   finalMealPlan = mpf.requestforsubstitute(mealsListDict, mealPlan, mealPicksVeg[-1], mealPicksMeat[-1])
    # grab ingredient list from weekly meal list
-   shoppingList = mpf.geningredientslist(mealsListDict, mealPlan)
-   print(shoppingList)
+   shoppingList = mpf.geningredientslist(mealsListDict, finalMealPlan)
+   #print(shoppingList) #for test
 
    #write list to file
    ll = open('foodlist.txt',"w")
    ll.write("**************Meals of the week****************\n")
    day = 0 
-   for items in mealPlan:
+   for items in finalMealPlan:
       ll.write("******"+dayOfTheWeek[day]+"*********\n")
       ll.write(items + "\n")
       day += 1
--- mealPlanFunctions.py
+++ mealPlanFunctions.py
@@ -35,6 +35,48 @@ def geningredientslist(mealsListDict, mealList):
 
    return ingredientsList
 
+def requestforsubstitute(mealsListDict, mealPlan, subMealVeg, subMealMeat):
+   """
+   provide option to substitute a meal from the random generated
+   mealPlan
+   """
+   #print meal plan with number options
+   finalMealPlan = mealPlan
+   i = 1
+   dayOfTheWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 
-
-
+   print('******* Preliminary MealPlan **********\n')
+   for meal in mealPlan:
+        print(str(i) + '. ' + dayOfTheWeek[i-1] + ' : ' + meal +'\n')
+        i += 1
+   print('\n')
+   #ask if a substitution is desired 
+   subChoice = input('Enter meal number to substitute or 99 to accept meal plan:')
+   subChoice = int(subChoice)
+   #check range of input
+   if subChoice == 99:
+      print('Meal Plan accepted, NO CHANGE made \n')
+      print('\n')
+   else:            
+      if subChoice <= 7:
+            for meal in mealsListDict:
+                if meal["name"] == mealPlan[subChoice-1]:
+                    #determine if meal is veg or non veg
+                    if meal["vegetarian"] == "yes":
+                        finalMealPlan[subChoice-1] = subMealVeg
+                    else:
+                        finalMealPlan[subChoice-1] = subMealMeat
+            print('Substituting ' + dayOfTheWeek[subChoice-1] + 'meal \n')
+            print('\n')
+      else:                  
+         print('Selection Not Valid, NO CHANGE to mealPlan made \n')
+         print('\n')
+        
+      #print out new list
+      print('******* Final MealPLan **********\n')
+      i = 0 
+      for meal in finalMealPlan:
+         print(dayOfTheWeek[i] + ' : ' + meal +'\n')
+         i += 1
+      print('\n')    
+   return finalMealPlan
